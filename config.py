ddd5436ddb5881f2ba3287c0cfc84a9dcd37d2c4
senavarrete <senavarrete@udec.cl>
2020-09-03 17:09:46 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://clandero:219UNIx2@152.74.52.187:5432/prototipo_shm'
-SQLALCHEMY_BINDS = {
-    # 'usuarios' : 'postgresql://clandero:219UNIx2@localhost/prototipo_shm.usuarios'
-}
-SECRET_KEY = '219UNIx2'
-
